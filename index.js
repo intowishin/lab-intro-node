622b865802543fcc9401a555f9807daf689279a1
Kamila Kowalczyk <kamila.kowalczyk@icloud.com>
2022-03-09 21:30:42 -0500

    iteration 2 working

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    this.length = this.items.push(item);
+    this.items.sort((a, b) => {
+      if (a < b) {
+        return -1;
+      }
+      if (a > b) {
+        return 1;
+      }
+      return 0;
+    });
+  }
 
   get(pos) {}
 
